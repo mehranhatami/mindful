8443622c93c69722118e2267ad7a405390ddff4d
Mehran Hatami <mehranhatamie@gmail.com>
2017-06-03 08:27:46 +0430

    use co and generators

--- server/controllers/auth_controller.js
+++ server/controllers/auth_controller.js
@@ -1,19 +1,23 @@
-const User = require('../models/user')
+const co = require('co');
+const User = require('../models/user');
 
 module.exports = {
-  authenticate(req, res, next) {
-    const username = req.body.username
-    const password = req.body.password
-    User.findOne({ username })
-      .then(user => {
-        if (!user) {
-          res.status(403).send('username not found')
-        } else if (user.password !== password) {
-          res.status(401).send('password is incorrect')
-        } else {
-          res.send(user.apiRepr())
-        }
-      })
-      .catch(next)
-  },
-}
+  authenticate: co(function * (req, res, next) {
+    try {
+      const username = req.body.username;
+      const password = req.body.password;
+      const user = yield User.findOne({ username });
+      if (!user) {
+        res.status(403).send('username not found');
+      } else if (user.password !== password) {
+        res.status(401).send('password is incorrect');
+      } else {
+        res.send(user.apiRepr());
+      }
+    } catch (err) {
+      next(err);
+    }
+  })
+};
+
+
